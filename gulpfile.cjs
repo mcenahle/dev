3a9d752e5d4c8380fcf93d865b35895f9026ec43
mcenahle <hi@mcenahle.page>
2026-09-06 17:19:21 +0800

    feat: 初始化网站

---
+++ gulpfile.cjs
@@ -0,0 +1,12 @@
+const uswds = require("@uswds/compile");
+
+uswds.settings.version = 3;
+
+uswds.paths.dist.theme = "./src/styles/uswds";
+uswds.paths.dist.css = "./public/uswds/css";
+uswds.paths.dist.js = "./public/uswds/js";
+uswds.paths.dist.fonts = "./public/uswds/fonts";
+uswds.paths.dist.img = "./public/uswds/img";
+
+exports.init = uswds.init;
+exports.compile = uswds.compile;
